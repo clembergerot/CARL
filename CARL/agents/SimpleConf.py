1719600a415000a4795218ec784661a6b26dfac4
clemence-itb <clemence.bergerot@charite.de>
2023-11-30 16:20:38 +0100

    Finish notebook TwoArmedBandit, start notebook RunExperiment

--- CARL/agents/SimpleConf.py
+++ CARL/agents/SimpleConf.py
@@ -39,7 +39,10 @@ class SimpleConf(object):
         """Computes chosen options from agents's probability table."""
         choices = np.zeros((np.shape(Ptable)[0]))  # 1 choice per agent
         rd = np.reshape(np.random.rand(len(choices)), (len(choices), 1))
-        choices = np.sum(rd > np.cumsum(Ptable, axis=1), axis=1)
+        # a random number between 0 and 1 is attributed to each agent
+        choices = np.sum(rd > np.cumsum(Ptable, axis=1), axis=1) # if 
+        # random number is smaller than agent's probability of choosing 0,
+        # then agent chooses 0
         choices = choices.astype(int)  # converts the choices to int values
         return choices
     
@@ -47,27 +50,31 @@ class SimpleConf(object):
         """Computes all agents' choices from their Qtable.
         Combines `compute_softmax` and `choose`.
         """
-        Ptable = self.compute_softmax(Qtable)
-        choices = self.choose(Ptable)
+        Ptable = self.compute_softmax(Qtable)  # converts Q-values to
+        # policy via softmax function
+        choices = self.choose(Ptable)  # choose an option according to
+        # policy
         return choices
     
     def update_Qvalues(self, G_att, choices, payoffs, Qtable): 
-        """Updates all agents' Q-values according to CARL, 
-        without for loops."""
+        """Updates all agents' Q-values according to CARL."""
         Qs = np.einsum('ijk->ikj', np.reshape(np.repeat(Qtable, self.N), 
                                               (self.N, self.M, self.N)))
         Rs = np.einsum('ijk->kij', np.reshape(np.repeat(np.repeat(payoffs, 
                                                                   self.N), 
                                                         self.M), 
                                               (self.N, self.M, self.N)))
-        deltas = Rs - Qs
-        # TODO: the cubes could be built at the beginning of the experiment
+        # dimensions: own Q-values, options, others' actions
+        deltas = Rs - Qs  # prediction errors
         alphac_cube = np.reshape(np.repeat(self.alphac, self.N*self.M), 
                                  (self.N, self.N, self.M))
         alphad_cube = np.reshape(np.repeat(self.alphad, self.N*self.M), 
                                  (self.N, self.N, self.M))
-        pos = deltas > 0
-        ags = np.arange(0, self.N, 1)
+        # 3d matrices with only confirmatory alphas vs. disconfirmatory
+        # alphas
+        pos = deltas > 0  # mask that indicates where pred errors are 
+        # positive
+        ags = np.arange(0, self.N, 1)  # vector of agent indices
         # choice_mask selects all actions that have been taken
         choice_mask = np.zeros((self.N, self.N, self.M)).astype(bool)
         choice_mask[:, ags, choices] = True
@@ -83,20 +90,21 @@ class SimpleConf(object):
         other_pos = pos & other_act
         same_neg = ~pos & same_act
         other_neg = ~pos & other_act
+        # alphas determines which alpha to attribute to each pred error
         alphas = np.zeros((self.N, self.N, self.M))
         alphas[same_pos] = alphac_cube[same_pos]
         alphas[same_neg] = alphad_cube[same_neg]
         alphas[other_neg] = alphac_cube[other_neg]
         alphas[other_pos] = alphad_cube[other_pos] 
-        # obs_mask selects actions that I observe; 
-        # TODO: could be implemented at the beginning of the experiment
+        # obs_mask selects actions that I observe
         obs_ij = np.asarray(nx.adjacency_matrix(G_att).todense()).astype(bool)
         obs_mask = np.reshape(np.repeat(obs_ij, self.M), (self.N, self.N, 
                                                           self.M))
-        np.fill_diagonal(obs_ij, True)
+        np.fill_diagonal(obs_ij, True)  # I observe my own actions
         obs_mask[:, :, 0] = obs_ij
         obs_mask[:, :, 1] = obs_ij
-        deltas[~obs_mask] = 0
+        deltas[~obs_mask] = 0  # cancelled pred errors from unobserved actions
+        # update Q-values according to CARL model
         deltas *= alphas
         deltas_sum = np.sum(deltas, axis=1)
         Qtable += deltas_sum
